e703c5b1fce7c4f39e6e8ec8a8f57de7408fc324
André Meneghelli Vale <andredalton@gmail.com>
2015-07-03 15:40:48 -0300

    Finalizando um sistema de instalação.
    [André, Marcello]

--- ocr/functions.py
+++ ocr/functions.py
@@ -2,4 +2,4 @@ from subprocess import call
 import os
 
 def call_tesseract(filename, ext):
-    call(["tesseract", "-l", "por", "-psm", "7", filename + ext, filename], stdout=open(os.devnull, 'wb'), stderr=open(os.devnull, 'wb'))
+    call(["tesseract", "-l", "por", "-psm", "8", filename + ext, filename], stdout=open(os.devnull, 'wb'), stderr=open(os.devnull, 'wb'))
--- setup.sh
+++ setup.sh
@@ -5,49 +5,30 @@
 ## or
 # chmod +x ./tesseract_install.sh && ./tesseract_install.sh
 
+sudo apt-get update
+sudo apt-get install python python-pip  python-dev
+sudo apt-get install tesseract-ocr tesseract-ocr-por
+sudo apt-get install libatlas-base-dev liblapack-dev git subversion python-virtualenv
+sudo pip install numpy scipy
+sudo apt-get remove ffmpeg x264 libx264-dev
+sudo apt-get -q install libopencv-dev build-essential checkinstall cmake pkg-config yasm libtiff4-dev libjpeg-dev \
+ libjasper-dev libavcodec-dev libavformat-dev libswscale-dev libdc1394-22-dev libxine-dev libgstreamer0.10-dev \
+ libgstreamer-plugins-base0.10-dev libv4l-dev python-dev python-numpy libtbb-dev libqt4-dev libgtk2.0-dev libfaac-dev \
+ libmp3lame-dev libopencore-amrnb-dev libopencore-amrwb-dev libtheora-dev libvorbis-dev libxvidcore-dev x264 v4l-utils \
+ ffmpeg
 
-sudo xargs -a requirements.system apt-get install
-
-pushd .
-mkdir ~/tesseract
-pushd ~/tesseract
-
-wget https://code.google.com/p/tesseract-ocr/downloads/detail?name=tesseract-ocr-3.02.02.tar.gz&can=2&q=
-wget https://code.google.com/p/tesseract-ocr/downloads/detail?name=tesseract-ocr-3.02.por.tar.gz&can=2&q=
-wget http://www.leptonica.org/source/leptonica-1.72.tar.gz
-
-tar zxf leptonica-1.72.tar.gz
-tar zxf tesseract-ocr-3.02.02.tar.gz
-tar zxf tesseract-ocr-3.02.por.tar.gz
-
-pushd leptonica-1.72
-./configure
-make
+version="$(wget -q -O - http://sourceforge.net/projects/opencvlibrary/files/opencv-unix | egrep -m1 -o '\"[0-9](\.[0-9])+' | cut -c2-)"
+sudo mkdir /tmp/OpenCV
+cd /tmp/OpenCV
+wget -O opencv.zip http://downloads.sourceforge.net/project/opencvlibrary/opencv-unix/"$version"/opencv-"$version".zip
+unzip opencv.zip
+cd opencv-"$version"
+mkdir build
+cd build
+cmake -D CMAKE_BUILD_TYPE=RELEASE -D WITH_TBB=ON -D BUILD_NEW_PYTHON_SUPPORT=ON -D WITH_V4L=ON -D INSTALL_C_EXAMPLES=ON \
+ -D INSTALL_PYTHON_EXAMPLES=ON -D BUILD_EXAMPLES=ON -D WITH_QT=ON -D WITH_OPENGL=ON ..
+make -j2
 sudo make install
 sudo ldconfig
-popd
 
-pushd tesseract-ocr
-./autogen.sh
-./configure
-make
-sudo make install
-sudo ldconfig
-popd
-
-popd
-
-
-## To install it in $HOME/local:
-#
-#./autogen.sh
-#./configure --prefix=$HOME/local/
-#make install
-#
-## To install it in $HOME/local using Leptonica libraries also installed in $HOME/local:
-#
-#./autogen.sh
-#LIBLEPT_HEADERSDIR=$HOME/local/include ./configure \
-#  --prefix=$HOME/local/ --with-extra-libraries=$HOME/local/lib
-#make install
-
+sudo cp ocr.traineddata /usr/share/tesseract-ocr/tessdata/
